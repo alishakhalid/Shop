e3cc0631e1301d178d3f204a32be64dbd806c6b2
alishakhalid <69908299+alishakhalid@users.noreply.github.com>
2021-10-06 17:03:43 +0200

    base for backend

---
+++ backendShop/src/main/java/com/example/backendShop/product/ProductWeb.java
@@ -0,0 +1,10 @@
+package com.example.backendShop.product;
+/**
+ * -----------------------------------------------------
+ * Author:          Alisha Khalid
+ * Datum:           Date
+ * Projekt:         project name
+ * Beschreibung:    Kurzbeschreibung
+ * ------------------------------------------------------
+ **/public class ProductWeb {
+}
